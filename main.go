b83e50978c8c33a9064fa52fec18317c51e28a1b
LuSrackhall <3647637206@qq.com>
2025-04-14 10:03:56 +0800

    移除嵌入文件的功能, 并通过绝对路径调用两个二进制文件以进行进一步的性能测试, 性能大大提升到了符合预期的程度

--- main.go
+++ main.go
@@ -1,65 +1,15 @@
 package main
 
 import (
-	"embed"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
-	"path/filepath"
 )
 
-//go:embed resource
-var resources embed.FS
-
-func prepareExecutable(resources embed.FS, tempDir, filename string) (string, error) {
-	execPath := filepath.Join(tempDir, filename)
-	execFile, err := os.Create(execPath)
-	if err != nil {
-		return "", fmt.Errorf("创建%s失败: %v", filename, err)
-	}
-	defer execFile.Close()
-
-	binFile, err := resources.Open("resource/" + filename)
-	if err != nil {
-		return "", fmt.Errorf("读取%s失败: %v", filename, err)
-	}
-	defer binFile.Close()
-
-	if _, err := io.Copy(execFile, binFile); err != nil {
-		return "", fmt.Errorf("复制%s失败: %v", filename, err)
-	}
-
-	if err := os.Chmod(execPath, 0755); err != nil {
-		return "", fmt.Errorf("设置%s执行权限失败: %v", filename, err)
-	}
-
-	return execPath, nil
-}
-
 func main() {
-	tempDir, err := os.MkdirTemp("", "im-select")
-	if err != nil {
-		fmt.Printf("创建临时目录失败: %v\n", err)
-		return
-	}
-	defer os.RemoveAll(tempDir)
+	imSelectPath := "/Users/srackhalllu/Desktop/资源管理器/safe/ims-mac/resource/im-select"
+	refreshPath := "/Users/srackhalllu/Desktop/资源管理器/safe/ims-mac/resource/refresh-input-focus"
 
-	// 准备im-select可执行文件
-	imSelectPath, err := prepareExecutable(resources, tempDir, "im-select")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-
-	// 准备refresh-input-focus可执行文件
-	refreshPath, err := prepareExecutable(resources, tempDir, "refresh-input-focus")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-
-	// 执行im-select
 	var cmd *exec.Cmd
 	if len(os.Args) > 1 {
 		err := exec.Command(imSelectPath, os.Args[1]).Run()
@@ -80,5 +30,4 @@ func main() {
 		}
 		fmt.Print(string(output))
 	}
-
 }
